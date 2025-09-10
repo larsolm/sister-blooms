7e350efff53ad9a2884e6f907e40e7ad8989fe89
larsolm <luke.larson@gmail.com>
2025-09-09 21:16:15 -0700

    fixed admin local dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/').pop()}/` : '/',
+  server: {
+    fs: {
+      strict: false
+    }
+  }
 })
